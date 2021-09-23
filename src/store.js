fa8d0ede70428f1adc339af1bf4f80dc0f5f8f95
Chawanzi Ng'uni <nwanzi5@gmail.com>
2021-09-23 12:11:27 +0200

    create new store.js to handle local storage

---
+++ src/store.js
@@ -0,0 +1,32 @@
+const setToLocalStorage = (toDo => {
+  localStorage.setItem('toDo', JSON.stringify(toDo))
+})
+
+const getFromLocalStorage = () => {
+  const toDo = localStorage.getItem('toDo')
+  return JSON.parse(toDo);
+}
+
+const checkboxState = () => {
+  const checkboxes = document.querySelectorAll('input[type=checkbox]');
+
+  checkboxes.forEach(checkbox => {
+    if (checkbox.checked) {
+      localStorage.setItem(checkbox.id, 'checked');
+    }
+
+    else {
+      localStorage.setItem(checkbox.id, 'unchecked');
+    }
+  })
+
+  checkboxes.forEach(checkbox => {
+    if (localStorage.getItem(checkbox.id) === 'checked') {
+      checkbox.checked = true;
+    } else {
+      checkbox.checked = false;
+    }
+  })
+}
+
+export { setToLocalStorage, getFromLocalStorage, checkboxState };
