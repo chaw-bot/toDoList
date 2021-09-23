8de44cfd98d2294a1b42b0883b079487b5cffca9
Chawanzi Ng'uni <nwanzi5@gmail.com>
2021-09-23 19:21:25 +0200

    fix linter errors in addremove.js

--- src/addremove.js
+++ src/addremove.js
@@ -2,29 +2,29 @@ import { setToLocalStorage, getFromLocalStorage, reloadToDo } from './store';
 import { refreshStore } from './status';
 
 const editToDo = () => {
-    const toDoList = document.getElementsByClassName('task');
-    for (let i = 0; i < toDoList.length; i += 1) {
-        const taskLabel = toDoList[i].children[0].children[1];
-        taskLabel.addEventListener('input', () => {
-            refreshStore();
-        });
-    }
+  const toDoList = document.getElementsByClassName('task');
+  for (let i = 0; i < toDoList.length; i += 1) {
+    const taskLabel = toDoList[i].children[0].children[1];
+    taskLabel.addEventListener('input', () => {
+      refreshStore();
+    });
+  }
 };
 
 const addToDo = () => {
-    const buttons = document.getElementsByClassName('delete');
-    for (let i = 0; i < buttons.length; i += 1) {
-        buttons[i].addEventListener('click', () => {
-            const index = `item-${i}`;
-            const inputItem = document.getElementsByName(index)[0];
-            inputItem.parentElement.parentElement.remove();
-            refreshStore();
-        });
-    }
+  const buttons = document.getElementsByClassName('delete');
+  for (let i = 0; i < buttons.length; i += 1) {
+    buttons[i].addEventListener('click', () => {
+      const index = `item-${i}`;
+      const inputItem = document.getElementsByName(index)[0];
+      inputItem.parentElement.parentElement.remove();
+      refreshStore();
+    });
+  }
 };
 
 const appendToDo = (task) => {
-    document.getElementById('list').insertAdjacentHTML('beforeend', `
+  document.getElementById('list').insertAdjacentHTML('beforeend', `
         <div class="task">
           <div class="checks">
             <input type="checkbox" name="item-${task.index}" readonly="true">
@@ -39,38 +39,38 @@ const appendToDo = (task) => {
 };
 
 const createToDo = (description) => {
-    const newTask = {
-        description,
-        completed: false,
-    };
+  const newTask = {
+    description,
+    completed: false,
+  };
 
-    const stateList = getFromLocalStorage();
-    const todoLength = stateList.length;
-    if (stateList.length === 0) {
-        newTask.index = 0;
-    } else {
-        newTask.index = todoLength;
-    }
+  const stateList = getFromLocalStorage();
+  const todoLength = stateList.length;
+  if (stateList.length === 0) {
+    newTask.index = 0;
+  } else {
+    newTask.index = todoLength;
+  }
 
-    stateList.push(newTask);
-    setToLocalStorage(stateList);
-    appendToDo(newTask);
-    reloadToDo();
-    editToDo();
-    addToDo();
+  stateList.push(newTask);
+  setToLocalStorage(stateList);
+  appendToDo(newTask);
+  reloadToDo();
+  editToDo();
+  addToDo();
 };
 
 document.querySelector('.input > input').addEventListener('keypress', (e) => {
-    if (e.key === 'Enter') {
-        createToDo(e.target.value)
-        e.target.value = '';
-    }
+  if (e.key === 'Enter') {
+    createToDo(e.target.value);
+    e.target.value = '';
+  }
 });
 
 document.getElementById('clear-all').addEventListener('click', () => {
-    const toDoList = document.getElementsByClassName('task');
-    [...toDoList].filter((toDoList) => toDoList.children[0].children[0].checked).forEach((item) => item.remove());
-    refreshStore();
+  const toDoList = document.getElementsByClassName('task');
+  [...toDoList].filter((toDoList) => toDoList.children[0].children[0].checked).forEach((item) => item.remove());
+  refreshStore();
 });
 
 export { addToDo, editToDo };
