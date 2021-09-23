4ba696eeee422c46bf93c17fd58d008e47f1f8ca
Chawanzi Ng'uni <nwanzi5@gmail.com>
2021-09-23 13:15:16 +0200

    add checkbox logic

--- src/status.js
+++ src/status.js
@@ -3,10 +3,10 @@ import { getFromLocalStorage, setToLocalStorage } from './store';
 const checkEvent = () => {
   const checkboxes = document.querySelectorAll('input[type=checkbox]');
 
-  for (let i = 0; i < checkboxes.length; i++) {
+  for (let i = 0; i < checkboxes.length; i += 1) {
     const task = checkboxes[i].nextElementSibling;
 
-    checkboxes[i].addEventListener("click", function () {
+    checkboxes[i].addEventListener('click', () => {
       if (checkboxes[i].checked) {
         task.style.textDecoration = 'line-through';
       } else {
@@ -17,6 +17,26 @@ const checkEvent = () => {
       setToLocalStorage(toDoList);
     });
   }
-}
+};
 
-export { checkEvent };
+const checkboxState = () => {
+  const checkboxes = document.querySelectorAll('input[type=checkbox]');
+
+  checkboxes.forEach((checkbox) => {
+    if (checkbox.checked) {
+      localStorage.setItem(checkbox.id, 'checked');
+    } else {
+      localStorage.setItem(checkbox.id, 'unchecked');
+    }
+  });
+
+  checkboxes.forEach((checkbox) => {
+    if (localStorage.getItem(checkbox.id) === 'checked') {
+      checkbox.checked = true;
+    } else {
+      checkbox.checked = false;
+    }
+  });
+};
+
+export { checkEvent, checkboxState };
