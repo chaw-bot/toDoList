0e9ee852f811d2fa4f5fc3bd57a7c269bd25ffdd
Chawanzi Ng'uni <nwanzi5@gmail.com>
2021-09-23 13:14:38 +0200

    move checkbox logic to status

--- src/store.js
+++ src/store.js
@@ -7,24 +7,4 @@ const getFromLocalStorage = () => {
   return JSON.parse(toDo);
 };
 
-const checkboxState = () => {
-  const checkboxes = document.querySelectorAll('input[type=checkbox]');
-
-  checkboxes.forEach((checkbox) => {
-    if (checkbox.checked) {
-      localStorage.setItem(checkbox.id, 'checked');
-    } else {
-      localStorage.setItem(checkbox.id, 'unchecked');
-    }
-  });
-
-  checkboxes.forEach((checkbox) => {
-    if (localStorage.getItem(checkbox.id) === 'checked') {
-      checkbox.checked = true;
-    } else {
-      checkbox.checked = false;
-    }
-  });
-};
-
-export { setToLocalStorage, getFromLocalStorage, checkboxState };
+export { setToLocalStorage, getFromLocalStorage };
